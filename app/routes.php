0548e45f59cdedaaaf1c443744bdf4b36116f064
indig3na <flaviodemello@gmail.com>
2016-12-13 16:39:33 +0100

    added routes to schoolyear

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,8 @@
 	
 	$w_routes = array(
 		['GET', '/', 'Default#home', 'default_home'],
-                ['GET', '/app/manage/country', 'Crud#country_get', 'crud_country_get'],
-                ['POST', '/app/manage/country', 'Crud#country_post', 'crud_country_post']
+		['GET', '/app/manage/country', 'Crud#country_get', 'crud_country_get'],
+		['POST', '/app/manage/country', 'Crud#country_post', 'crud_country_post'],
+		['GET', '/app/manage/schoolyear', 'Crud#schoolyear_get', 'crud_schoolyear_get'],
+		['POST', '/app/manage/schoolyear', 'Crud#schoolyear_post', 'crud_schoolyear_post']
 	);
