b32ad9403c0b1cb5d1d25c2d135069ee956454d9
ceilenbecker <ceilenbecker@gmail.com>
2016-12-13 17:07:16 +0100

    addeed views charel

---
+++ app/Views/crud/country.php
@@ -0,0 +1,20 @@
+<?php $this->layout('layout', ['title' => 'Accueil']) ?>
+
+<?php $this->start('main_content') ?>
+<table>
+    <thead>
+    <h2>Country</h2>
+    </thead>
+    <tbody>
+        <tr>
+            <th></th>
+        </tr>
+        <tr>   
+            <form></form>
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+<?php $this->stop('main_content') ?>
--- app/Views/layout.php
+++ app/Views/layout.php
@@ -29,8 +29,6 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
                 integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
                 crossorigin="anonymous"></script>
-        <!--Google Maps API-->
-        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnycWatbGyK6ldFqErjFtko1yeMclNUOA&amp;sensor=true"></script>
 
         <script type="text/javascript" src="<?= $this->assetUrl('js/script.js')?>"></script>
     </body>
