515440baf417bc0774713425ae03d7c4def854aa
Flavio de Melo <flaviodemello@gmail.com>
2016-12-13 19:29:13 +0100

    added crud controler schoolyear

--- app/Controller/CrudController.php
+++ app/Controller/CrudController.php
@@ -21,4 +21,20 @@ class CrudController extends ControllerTemplate
 		$this->show('crud/country');
 	}
 
+	/**
+	 * Page de gestion CRUD pour table schoolyear en GET
+	 */
+	public function schoolyear_get()
+	{
+		$this->show('crud/schoolyear');
+	}
+        
+	/**
+	 * Page de gestion CRUD pour table schoolyear en POST
+	 */
+	public function schoolyear_post()
+	{
+		$this->show('crud/schoolyear');
+	}
+
 }
--- app/routes.php
+++ app/routes.php
@@ -5,8 +5,9 @@
 <<<<<<< HEAD
 		['GET', '/app/manage/country', 'Crud#country_get', 'crud_country_get'],
 		['POST', '/app/manage/country', 'Crud#country_post', 'crud_country_post'],
+		//SchoolYear
 		['GET', '/app/manage/schoolyear', 'Crud#schoolyear_get', 'crud_schoolyear_get'],
-		['POST', '/app/manage/schoolyear', 'Crud#schoolyear_post', 'crud_schoolyear_post']
+		['POST', '/app/manage/schoolyear', 'Crud#schoolyear_post', 'crud_schoolyear_post'],
 =======
         //Country
         ['GET', '/app/manage/country', 'Crud#country_get', 'crud_country_get'],
