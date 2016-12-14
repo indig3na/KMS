f4cf04112bbab8a4fe2251a794bffd13c0f4102b
indig3na <flaviodemello@gmail.com>
2016-12-14 09:19:30 +0100

    correction form

--- app/Views/crud/schoolyear.php
+++ app/Views/crud/schoolyear.php
@@ -3,14 +3,13 @@
 <?php $this->start('main_content') ?>
     <table>
         <thead>
-        <h2>Année Scolaire</h2>
+            <h2>Année Scolaire</h2>
         </thead>
         <tbody>
         <tr>
             <th>Année en Cours</th>
         </tr>
         <tr>
-            <form></form>
         </tr>
         <tr>
             <td></td>
