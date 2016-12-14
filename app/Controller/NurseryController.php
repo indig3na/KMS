9d54b1639d4822acfca1c062bd6e5a457847653f
Flavio de Melo <flaviodemello@gmail.com>
2016-12-14 17:08:40 +0100

    ajout et modif sur schoolyear et nursery

--- app/Controller/NurseryController.php
+++ app/Controller/NurseryController.php
@@ -29,7 +29,37 @@ class NurseryController extends ControllerTemplate
      */
     public function nursery_post()
     {
-        $this->show('crud/crud');
-    }
+        $name = isset($_POST['nur_name']) ? trim(strip_tags($_POST['nur_name'])) : '';
+        $address = isset($_POST['nur_address']) ? trim(strip_tags($_POST['nur_address'])) : '';
+        $email = isset($_POST['nur_email']) ? trim(strip_tags($_POST['nur_email'])) : '';
+        $telephone = isset($_POST['nur_telephone']) ? trim(strip_tags($_POST['nur_telephone'])) : '';
+        $website = isset($_POST['nur_website']) ? trim(strip_tags($_POST['nur_website'])) : '';
 
+        //validation & insertion données
+        $errorList = array();
+        $nurseryOk = true;
+
+        if (empty($name) || empty($address) || empty($email) || empty($telephone) || empty($website)) {
+            echo 'champ vide, remplir tous les champs obligatoires!<br>';
+            $nurseryOk = false;
+        }
+        if ($nurseryOk) {
+            $nurseryModel = new nurseryModel();
+            //Insert data
+            $tableData = $nurseryModel->insert(array(
+                'nur_name' => $name,
+                'nur_address' => $address,
+                'nur_email' => $email,
+                'nur_telephone' => $telephone,
+                'nur_website' => $website
+            ));
+            // if not inserted error
+            if ($tableData === false) {
+                $errorList[] = 'Insertion Error<br>';
+            }
+        }
+        $this->showJson(['Succès / Erreur']);
+
+    }
 }
+
