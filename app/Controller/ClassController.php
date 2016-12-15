8e1a661117824db20790a37f75791ff66341d028
indig3na <flaviodemello@gmail.com>
2016-12-15 16:01:03 +0100

    test class sans program

--- app/Controller/ClassController.php
+++ app/Controller/ClassController.php
@@ -9,7 +9,7 @@
 namespace Controller;
 
 use Model\ClassModel;
-use Model\ProgramModel;
+//use Model\ProgramModel;
 use Model\SchoolYearModel;
 
 
@@ -20,23 +20,97 @@ class ClassController extends ControllerTemplate{
     public function class_get()
     {
         $model = new ClassModel();
-        $tabledata = $model->findAllColumns(['cls_id', 'cls_name', 'program_prg_id', 'school_year_scy_id' ]);
+        $tabledata = $model->findAllColumns(['cls_id', 'cls_name', /*'program_prg_id',*/ 'school_year_scy_id' ]);
 
         //initialisation of $fkdata
         $fkData = array();
         //Pour chaque Foreign key, initialiser le modèle et stocker la table de valeurs
-        $programModel = new ProgramModel();
+        //$programModel = new ProgramModel();
         $schoolYearModel = new SchoolYearModel();
-        $fkData['program_prg_id'] = $programModel->findIndexedColumn('prg_name');
-        $fkData['schoolyear_scy_id'] =  $schoolYearModel->findIndexedColumn('scy_name');
+        //$fkData['program_prg_id'] = $programModel->findIndexedColumn('prg_name');
+        $fkData['school_year_scy_id'] =  $schoolYearModel->findIndexedColumn('scy_year');
         $vars = [
             'title' => 'Class',
-            'header' => ['Class', 'Program', 'SchoolYear'],
+            'header' => ['Class', /*'Program',*/ 'SchoolYear'],
             'primaryKey' => 'cls_id',
             'data' => $tabledata,
             'fkData' => $fkData
         ];
         $this->show('crud/crud', $vars);
+    }
+    /**
+     * Page de gestion CRUD pour table Class en POST
+     */
+    public function class_post()
+    {
+        $name = isset($_POST['cls_name']) ? trim(strip_tags($_POST['cls_name'])) : '';
+       // $program = isset($_POST['program_prg_id']) ? trim(strip_tags($_POST['program_prg_id'])) : '';
+        $schoolyear = isset($_POST['school_year_scy_id']) ? trim(strip_tags($_POST['school_year_scy_id'])) : '';
+        $method = $_POST['method'];
+        $id = '';
+
+        //validation
+        $succesList = array();
+        $success = false;
+        $errorList = array();
+        $data = array();
+
+        if (empty($name) || /*empty($program) ||*/ empty($schoolyear)) {
+            $errorList[] = 'All champs are required';
+            $success = false;
+        }
+        $data = [
+            'cls_name' => $name,
+            //'program_prg_id' => $program,
+            'school_year_scy_id' => $schoolyear
+        ];
+        // if input ok
+        if (empty($errorList)) {
+
+            $classModel = new ClassModel();
+            //Insert
+            if ($method === 'insert') {
+                $tableData = $classModel->insert($data);
+                // if not inserted error
+                if ($tableData === false) {
+                    $errorList[] = 'Insert Error';
+                } else {
+                    $succesList[] = 'Class inserted';
+                    $success = true;
+                }
+            }//update
+            elseif ($method === 'update') {
+                $id = $_POST['id'];
+                $updateData = $classModel->update($data, $id, $stripTags = true);
+                // if not updated error
+                if ($updateData === false) {
+                    $errorList[] = 'Update Error';
+                } else {
+                    $succesList[] = 'Class Updated';
+                    $success = true;
+                }
+            }
+        }
+        //delete
+        if ($method === 'delete') {
+            $id = $_POST['id'];
+            $classModel = new ClassModel();
+            $deletedData = $classModel->delete($id);
+            if ($deletedData === false) {
+                $errorList[] = 'Deletion Error';
+            } else {
+                $succesList[] = 'Class Deleted';
+                $success = true;
+            }
+        }
+        // show json errorList and/or successList message
+        if ($success) {
+            $this->showJson(['code' => 1, 'message' => implode('
+            ', $succesList)]);
+        } else {
+            $this->showJson(['code' => 0, 'message' => implode('
+            ', $errorList)]);
+        }
     }
 
 }
