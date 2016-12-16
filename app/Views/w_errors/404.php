2b36ab75573e6b415d6fb7a0b376db977d099b16
indig3na <flaviodemello@gmail.com>
2016-12-16 12:13:47 +0100

    404/403

--- app/Views/w_errors/404.php
+++ app/Views/w_errors/404.php
@@ -1,5 +1,12 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+
+<?php $this->layout('layout', ['title' => '404']) ?>
 
 <?php $this->start('main_content'); ?>
-<img src=\assets\img">
+<h2>404! Page Not Found</h2>
+<div>
+    <img src="<?= $this->assetUrl('img/404.jpg') ?>" height="800" width="1100" alt="" />
+    <div>
+        <a href="<?= $this->url('default_home') ?>"> BACK TO Home </a>
+    </div>
+</div>
 <?php $this->stop('main_content'); ?>
