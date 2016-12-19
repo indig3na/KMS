933f12bd994a7701fe1b5052ce98556648c19f5e
Flavio de Melo <flaviodemello@gmail.com>
2016-12-19 19:18:35 +0100

    add

--- app/Views/Rapports/dailyrapport.php
+++ app/Views/Rapports/dailyrapport.php
@@ -1,34 +1,108 @@
 <?php $this->layout('layoutsystem', ['title' => $title]) ?>
 
 <?php $this->start('main_content') ?>
-<h2>My day today</h2>
 
-<p>name:</p>
-<p>date:</p>
+<div style="" class="container">
+    <h1 style="text-align: center;">My Day today</h1>
+    <p>name:</p>
+    <p>date:</p>
+    <div class="menu1">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <a href="" id="manger" class="btn btn-block btn-primary ">Manger</a>
+            <a href="" id="fisio" class="btn btn-block btn-primary ">Wet / Poo</a>
+            <a href="" id="sieste" class="btn btn-block btn-primary ">Sieste</a>
+            <a href="" id="comments" class="btn btn-block btn-primary ">Comments</a>
+        </div>
+    </div>
 
-<form action="" method="get">
+    <div style="display: none;" id="optionManger">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <a href="" id="matin" class="btn btn-block btn-primary ">Guter matin</a>
+            <a href="" id="midi" class="btn btn-block btn-primary ">Repas midi</a>
+            <a href="" id="aprésmidi" class="btn btn-block btn-primary ">Guter aprés-midi</a>
+        </div>
+    </div>
 
-    <h3> I was</h3>
-    <input type="radio" name="answer1" value="blue" checked="checked">blue<br>
-    <input type="radio" name="answer1" value="red">red<br>
-    <input type="radio" name="answer1" value="green">green<br>
+    <div style="display: none;" id="quant">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <a href="" id="tBien" class="btn btn-block btn-primary ">Três bien</a>
+            <a href="" id="bien" class="btn btn-block btn-primary ">Bien</a>
+            <a href="" id="mal" class="btn btn-block btn-primary ">Mal</a>
+        </div>
+    </div>
 
-    <h3>Q2. The sum of two and three equals...</h3>
-    <input type="radio" name="answer2" value="seven">seven<br>
-    <input type="radio" name="answer2" value="six">six<br>
-    <input type="radio" name="answer2" value="five" checked="checked" >five<br>
+    <div style="display: none;" id="optionFisio">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <a href="" id="wet" class="btn btn-block btn-primary ">J'ai fais Pipi</a>
+            <a href="" id="midi" class="btn btn-block btn-primary ">J'ai fais Caca</a>
+        </div>
+    </div>
 
-    <h3>Q3. Which of these animals is NOT an mammal.</h3>
-    <input type="radio" name="answer3" value="lizard" checked="checked">lizard<br>
-    <input type="radio" name="answer3" value="monkey">monkey<br>
-    <input type="radio" name="answer3" value="dog">dog<br>
+    <div style="display: none;" id="optionSieste">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <a href="" id="45m" class="btn btn-block btn-primary ">Sieste 45 min</a>
+            <a href="" id="60m" class="btn btn-block btn-primary ">Sieste 60 min</a>
+            <a href="" id="90m" class="btn btn-block btn-primary ">Sieste 90 min</a>
+        </div>
+    </div>
 
-    <h3>Q4. Santa Claus is most associated with this holiday.</h3>
-    <input type="radio" name="answer4" value="Halloween">Halloween<br>
-    <input type="radio" name="answer4" value="Christmas"checked="checked">Christmas<br>
-    <input type="radio" name="answer4" value="Fourth of July">Forth of July<br>
-    <br>
-    <input type="submit" value="Submit">
-    <input type="reset" value="Reset">
+    <div style="display: none;" id="text">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+            <h4>Only fill if no option available on the Maim Menu</h4>
+            <textarea rows="10" cols="151" placeholder="Comments here..."></textarea>
+            <button id="submitComments" type="submit" class="btn btn-block btn-primary">Submit</button>
+        </div>
+    </div>
 
+    <a  id="home" style="display: none;" href=""> BACK TO Home </a>
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+    $( "#manger" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( ".menu1" ).hide( "drop", { direction: "down" }, "fast" );
+        $( "#optionManger, #home" ).show( "fast" );
+    });
+
+    $( "#matin" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( "#optionManger" ).hide( "drop", { direction: "down" }, "fast" );
+        $( "#quant, #home" ).show( "fast" );
+    });
+
+    $( "#fisio" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( ".menu1" ).hide( "drop", { direction: "down" }, "fast" );
+        $( "#optionFisio, #home" ).show( "fast" );
+    });
+
+    $( "#sieste" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( ".menu1" ).hide( "drop", { direction: "down" }, "fast" );
+        $( "#optionSieste, #home" ).show( "fast" );
+    });
+
+    $( "#comments" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( ".menu1" ).hide( "drop", { direction: "down" }, "fast" );
+        $( "#text, #home" ).show( "fast" );
+    });
+
+    $( "#submitComments" ).click(function(event) {
+        event.preventDefault();
+        //console.log("click");
+        $( "#text, #home" ).hide( "drop", { direction: "down" }, "fast" );
+        $( ".menu1" ).show( "fast" );
+    });
+
+</script>
 <?php $this->stop('main_content') ?>
